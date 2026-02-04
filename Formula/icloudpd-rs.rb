9a12815bc2df7b9b326acdca46a7ffa36abf2383
Rob Hooper <rhoopr@users.noreply.github.com>
2026-02-03 22:43:50 -0500

    v0.1.0: add formula with release checksums

--- Formula/icloudpd-rs.rb
+++ Formula/icloudpd-rs.rb
@@ -7,20 +7,20 @@ class IcloudpdRs < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/rhoopr/icloudpd-rs/releases/download/v#{version}/icloudpd-rs-macos-aarch64.tar.gz"
-      sha256 "PLACEHOLDER_AARCH64"
+      sha256 "ac78d543f9fe6defb839aaa4b817e963c41b12e01dbc02b5404a8f1d67c92103"
     else
       url "https://github.com/rhoopr/icloudpd-rs/releases/download/v#{version}/icloudpd-rs-macos-x86_64.tar.gz"
-      sha256 "PLACEHOLDER_X86_64"
+      sha256 "e0553b00d040e571af2601057fa84c26c6dde33f6b522e8db91f6c786f28332f"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/rhoopr/icloudpd-rs/releases/download/v#{version}/icloudpd-rs-linux-aarch64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_AARCH64"
+      sha256 "438972ed98dfb20fdb3a8b88e903e70512616fdac2b1de1ebc8372a1f0c89fd3"
     else
       url "https://github.com/rhoopr/icloudpd-rs/releases/download/v#{version}/icloudpd-rs-linux-x86_64.tar.gz"
-      sha256 "PLACEHOLDER_LINUX_X86_64"
+      sha256 "dbb274ad0b7154f038a8d36e3daaffe49f8148cad1f3a09b22a90fd006138016"
     end
   end
 
--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-# icloudpd-rs-tap
+# homebrew-icloudpd-rs
 
 Homebrew tap for [icloudpd-rs](https://github.com/rhoopr/icloudpd-rs).
 
 ## Installation
 
 ```bash
-brew tap rhoopr/icloudpd-rs-tap
+brew tap rhoopr/icloudpd-rs
 brew install icloudpd-rs
 ```
 
@@ -14,4 +14,20 @@ brew install icloudpd-rs
 ```bash
 brew update
 brew upgrade icloudpd-rs
+```
+
+## From Source
+
+If you prefer to build from source:
+
+```bash
+brew install --HEAD icloudpd-rs
+```
+
+Or manually:
+
+```bash
+git clone https://github.com/rhoopr/icloudpd-rs.git
+cd icloudpd-rs
+cargo build --release
 ```
