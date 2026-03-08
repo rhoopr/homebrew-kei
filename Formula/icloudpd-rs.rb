7f7933fb4a2dae7f00635e407a43f4de32e8ac30
Rob Hooper <github@robhooper.xyz>
2026-03-07 20:23:51 -0500

    icloudpd-rs 0.3.0
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/icloudpd-rs.rb
+++ Formula/icloudpd-rs.rb
@@ -1,13 +1,13 @@
 class IcloudpdRs < Formula
   desc "Fast, reliable iCloud Photos downloader written in Rust"
   homepage "https://github.com/rhoopr/icloudpd-rs"
-  version "0.2.1"
+  version "0.3.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/rhoopr/icloudpd-rs/releases/download/v0.2.1/icloudpd-rs-macos-aarch64.tar.gz"
-      sha256 "988a8c4b5d63a9ba4523cb37ff672ca4dbacc50d445fafcbf1153fcee99fcb78"
+      url "https://github.com/rhoopr/icloudpd-rs/releases/download/v0.3.0/icloudpd-rs-macos-aarch64.tar.gz"
+      sha256 "3648e1b248c71f90a19ead2ce1328ac7aeda75b2fb9ce2da2d4a6448607afd54"
     else
       url "https://github.com/rhoopr/icloudpd-rs/releases/download/v#{version}/icloudpd-rs-macos-x86_64.tar.gz"
       sha256 "241d2e9822e2167a3e4fd968b3c30559d0ff71ecf925c9b2b74ebce1a8b9e580"
