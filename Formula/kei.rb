c8c977ec71294622831bc9768296cd9467389a04
Rob Hooper <github@robhooper.xyz>
2026-04-01 16:14:42 -0400

    Fix kei formula: correct URLs, checksums, and binary name for v0.5.0

--- Formula/kei.rb
+++ Formula/kei.rb
@@ -6,26 +6,26 @@ class Kei < Formula
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/rhoopr/kei/releases/download/v0.5.0/kei-macos-aarch64.tar.gz"
+      url "https://github.com/rhoopr/kei/releases/download/v#{version}/kei-macos-aarch64.tar.gz"
       sha256 "c62e41bf50059c4f38486210ffcdb0b25decab11de364fcf1faebb7fa18b657c"
     else
-      url "https://github.com/rhoopr/kei/releases/download/v#{version}/icloudpd-rs-macos-x86_64.tar.gz"
-      sha256 "241d2e9822e2167a3e4fd968b3c30559d0ff71ecf925c9b2b74ebce1a8b9e580"
+      url "https://github.com/rhoopr/kei/releases/download/v#{version}/kei-macos-x86_64.tar.gz"
+      sha256 "ad3f35ba191f9af4324ed310165570e8a9e6d6260091d12cddd93eecd93b90be"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/rhoopr/kei/releases/download/v#{version}/icloudpd-rs-linux-aarch64.tar.gz"
-      sha256 "0ceb55dab745486bf3329b078876244048b30ad001cc8e6db338cf988837e88a"
+      url "https://github.com/rhoopr/kei/releases/download/v#{version}/kei-linux-aarch64.tar.gz"
+      sha256 "64e57d92267d5056a0792aab02b2370001f7ad6f2ac9ff7374280ddf073d99cb"
     else
-      url "https://github.com/rhoopr/kei/releases/download/v#{version}/icloudpd-rs-linux-x86_64.tar.gz"
-      sha256 "674edd4bd6e8e05e419dd0c7b19db210f59a9c81ae5765341b4d7db96dd6a44d"
+      url "https://github.com/rhoopr/kei/releases/download/v#{version}/kei-linux-x86_64.tar.gz"
+      sha256 "2a7b1d9cd8f4d01642c041944894daabff4c30bc0e54d337ad62efe256c83015"
     end
   end
 
   def install
-    bin.install "icloudpd-rs" => "kei"
+    bin.install "kei"
   end
 
   test do
