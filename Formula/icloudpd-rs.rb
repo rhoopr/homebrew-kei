2b9e74da28204ac12c1abb3489c348872608ed68
Rob Hooper <github@robhooper.xyz>
2026-04-01 15:20:38 -0400

    Replace icloudpd-rs formula with deprecation shim pointing to kei

--- Formula/icloudpd-rs.rb
+++ Formula/icloudpd-rs.rb
@@ -1,34 +1,12 @@
 class IcloudpdRs < Formula
-  desc "Fast, reliable iCloud Photos downloader written in Rust"
-  homepage "https://github.com/rhoopr/icloudpd-rs"
+  desc "Renamed to kei - install with: brew install rhoopr/kei/kei"
+  homepage "https://github.com/rhoopr/kei"
   version "0.4.2"
   license "MIT"
 
-  on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/rhoopr/icloudpd-rs/releases/download/v0.4.2/icloudpd-rs-macos-aarch64.tar.gz"
-      sha256 "be5b0596092baeeb6c01d3c5570b73fad71facdc9ddee941a330e7bc96cc9c21"
-    else
-      url "https://github.com/rhoopr/icloudpd-rs/releases/download/v#{version}/icloudpd-rs-macos-x86_64.tar.gz"
-      sha256 "241d2e9822e2167a3e4fd968b3c30559d0ff71ecf925c9b2b74ebce1a8b9e580"
-    end
-  end
-
-  on_linux do
-    if Hardware::CPU.arm?
-      url "https://github.com/rhoopr/icloudpd-rs/releases/download/v#{version}/icloudpd-rs-linux-aarch64.tar.gz"
-      sha256 "0ceb55dab745486bf3329b078876244048b30ad001cc8e6db338cf988837e88a"
-    else
-      url "https://github.com/rhoopr/icloudpd-rs/releases/download/v#{version}/icloudpd-rs-linux-x86_64.tar.gz"
-      sha256 "674edd4bd6e8e05e419dd0c7b19db210f59a9c81ae5765341b4d7db96dd6a44d"
-    end
-  end
+  depends_on "rhoopr/kei/kei"
 
   def install
-    bin.install "icloudpd-rs"
-  end
-
-  test do
-    assert_match "Download iCloud photos", shell_output("#{bin}/icloudpd-rs --help")
+    odie "icloudpd-rs has been renamed to kei. Run: brew install rhoopr/kei/kei"
   end
 end
