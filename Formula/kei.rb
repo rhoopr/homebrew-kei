8d490b4ccc2af9d93f2c64af2b49cd0bae321c36
Rob Hooper <github@robhooper.xyz>
2026-04-02 11:03:03 -0400

    kei 0.5.1
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/kei.rb
+++ Formula/kei.rb
@@ -1,13 +1,13 @@
 class Kei < Formula
   desc "Photo sync engine - compact, efficient, reliable"
   homepage "https://github.com/rhoopr/kei"
-  version "0.5.0"
+  version "0.5.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/rhoopr/kei/releases/download/v#{version}/kei-macos-aarch64.tar.gz"
-      sha256 "c62e41bf50059c4f38486210ffcdb0b25decab11de364fcf1faebb7fa18b657c"
+      url "https://github.com/rhoopr/kei/releases/download/v0.5.1/kei-macos-aarch64.tar.gz"
+      sha256 "690a0d2cc0fbaff47253912a993dccb21437823d1668e50d6a1563fd9893a250"
     else
       url "https://github.com/rhoopr/kei/releases/download/v#{version}/kei-macos-x86_64.tar.gz"
       sha256 "ad3f35ba191f9af4324ed310165570e8a9e6d6260091d12cddd93eecd93b90be"
